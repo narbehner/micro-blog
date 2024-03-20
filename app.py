171b4bb30aa975519f5e265c51dd2ad808d5e7b5
narbehner <115324797+narbehner@users.noreply.github.com>
2024-03-20 17:32:31 -0400

    Add files via upload
    connected to mongoDB, hopefully fixes gunicorn issue

--- app.py
+++ app.py
@@ -11,22 +11,19 @@ def create_app():
     client = MongoClient(os.getenv("MONGODB_URI"))
     app.db= client.microblog           
 
-    entries = []
-
     @app.route("/", methods=["GET", "POST"])            #endpoint registrations      
     def home():
         if request.method == "POST":       
             entry_content = request.form.get("content")
             date = datetime.datetime.today().strftime("%Y-%m-%d")
-            entries.append((entry_content,date))
             app.db.entries.insert_one({"content": entry_content, "date": date})
 
         entries_with_date = [
-            (entry[0],
-            entry[1],
-            datetime.datetime.strptime(entry[1],"%Y-%m-%d").strftime("%b %d")
+            (entry["content"],
+            entry["date"],
+            datetime.datetime.strptime(entry["date"],"%Y-%m-%d").strftime("%b %d")
             )
-            for entry in entries
+            for entry in app.db.entries.find({})
         ]
         return render_template("home.html", entries=entries_with_date)
     
